d83b53fb391d62ba153680e10bd6cf6383274620
Juan José Villegas <jj.villegas47@gmail.com>
2018-11-22 21:48:45 -0500

    Fixed schema

--- schemas/ZoneFee.js
+++ schemas/ZoneFee.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const zoneFeeModel = new Schema({
-    fee: number,
-    zone: string
+    fee: Number,
+    zone: String
 });
 
 module.exports = mongoose.model('ZoneFee', zoneFeeModel);
