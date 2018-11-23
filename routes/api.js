63ff6f37a4988a291672bd795f6a64c4dcc11075
Juan José Villegas <jj.villegas47@gmail.com>
2018-11-22 21:57:48 -0500

    update or create

--- routes/api.js
+++ routes/api.js
@@ -5,9 +5,19 @@ const router = express.Router();
 
 //Booking api
 router.post('/zonefee', (req, res) => {
-  let zoneFee = new ZoneFee(req.body);
-  zoneFee.save();
-  res.status(201).send(zoneFee);
+
+  ZoneFee.findOne({ zone: req.body.zone }, (err, zoneFee) => {
+    if (zoneFee) {
+      zoneFee.fee = req.body.fee;
+      res.status(201).send(zoneFee);
+    } else {
+      let newZoneFee = new ZoneFee(req.body);
+      newZoneFee.save();
+      res.status(201).send(newZoneFee);
+    }
+  });
+
+  
 });
 
 module.exports = router;
